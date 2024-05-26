593052c954179e08fca5d9adab4c043923bb5816
hungaa1 <nguyentronghung05072003@gmail.com>
2024-05-26 23:38:55 +0700

    ok

---
+++ src/app/assets/images/index.tsx
@@ -0,0 +1,16 @@
+import theme from "./loginLogo.png";
+import theme1 from "./theme1.jpg";
+import theme2 from "./theme2.jpg";
+import theme3 from "./theme3.png";
+import theme4 from "./theme4.webp";
+import theme5 from "./theme5.jpg";
+import theme6 from "./theme6.jpg";
+const loginLogo = [
+  { id: 1, theme: [theme, theme1] },
+  { id: 2, theme: [theme2, theme3] },
+  { id: 3, theme: [theme4, theme5] },
+];
+const Images = {
+  loginLogo,
+};
+export default Images;
